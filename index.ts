1d2b3eef9d03b97e2a0e0d1bea799aed452c4064
Ondrej Machala <o.machala@gmail.com>
2022-05-28 21:04:31 +0100

    variable port

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import server from './src/server';
 
-server.listen(3000, () => {
-  console.log('server up, http://localhost:3000');
+const { PORT: port = 3000 } = process.env;
+
+server.listen(Number(port), () => {
+  console.log(`server up, http://localhost:${port}`);
 });
